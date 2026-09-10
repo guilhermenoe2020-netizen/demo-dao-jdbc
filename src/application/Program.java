9aeb0d24a12d685d38d055d12d4700c3f49ac4b7
Guilherme Noe <guilhermenoe2020@gmail.com>
2026-09-09 21:40:30 -0400

    SellerDaoJDBC and DaoFactory updated

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
+
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +17,10 @@ public class Program {
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
 
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(seller);
 
 	}
 
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,6 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
-	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleById(Integer id);
+	Department findAll(Integer id);
+	List<Department> findAll();
 }
