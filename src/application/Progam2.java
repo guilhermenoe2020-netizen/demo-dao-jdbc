d5be7fbcf2284ac258ad0e138b868366a4c471e1
Guilherme Noe <guilhermenoe2020@gmail.com>
2026-09-12 18:56:49 -0400

    Implement insert method in DepartmentDaoJDBC

--- src/application/Progam2.java
+++ src/application/Progam2.java
@@ -15,35 +15,39 @@ public class Progam2 {
 		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+	
 		
-		System.out.println("\n== test 1: findAll ==");
-		List<Department> list = departmentDao.findAll();
-	    
-		for(Department dep : list) {
-			System.out.println(dep);
-		}
-		
-		System.out.println("\n== test 2: findById ==");
+		System.out.println("\n== test 1: findById ==");
 		Department department = departmentDao.findById(1);
 		System.out.println(department);
 		
 		
-		System.out.println("\n== test 3: update ==");
+		System.out.println("\n== test 2: findAll ==");
+		List<Department> list = departmentDao.findAll();
+		for(Department dep : list) {
+			System.out.println(dep);
+		}
+		
+		
+		System.out.println("\n== test 3: insert ==");
+		Department newDepartment = new Department(null, "computers");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		
+		System.out.println("\n== test 4: update ==");
 		department = departmentDao.findById(1);
 		department.setName("pc");
 		departmentDao.update(department);
 		System.out.println("Update completed");
 		
 		
-		
-		
-		
-		
-		System.out.println("\n== test 4 : delete ==");
+		System.out.println("\n== test 5: delete ==");
 		System.out.println("Enter id for delete test");
 		int id = sc.nextInt();
 		departmentDao.deleteById(id);
 		System.out.println("delet completed");
+		
 		sc.close();
 	}
 }
